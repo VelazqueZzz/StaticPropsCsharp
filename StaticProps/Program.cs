25d95170f164613bd6e09832b2495f6fec87a9b6
ITaly177 Milan <patrokl2010@yandex.ru>
2023-09-11 10:31:25 +0300

    Add project files.

---
+++ StaticProps/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Fibs
+{
+    private static int last = 1;
+    private static int prev = 1;
+    public static int number
+    {
+        get {
+            int res = prev;
+            last = last + prev;
+            prev = last - prev ;
+            return res;
+        }
+        set
+        {
+            prev = 1;
+            last = 1;
+            for(int k = 2; k <= value; k++)
+            {
+                last = last + prev;
+                prev = last - prev;
+            }
+        }
+    }
+}
+class StaticPropsDemo
+{
+    static void Main()
+    {
+        for(int k = 1; k <= +10; k++)
+        {
+            Console.Write("{0,4}", Fibs.number);
+        }
+        Console.WriteLine();
+        Fibs.number = 6;
+        for(int k = 1; k <= 10; k++)
+        {
+            Console.Write("{0,4}",Fibs.number);
+        }
+        Console.WriteLine();
+        Fibs.number = 1;
+        for(int k = 1; k <= 10; k++){
+            Console.Write("{0,4}", Fibs.number);
+        }
+        Console.WriteLine();
+    }
+}
